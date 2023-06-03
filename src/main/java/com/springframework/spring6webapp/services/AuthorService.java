2da7fca78c1a7fd91bb1eb4e7cfb9b3ba202dabf
Troy254 <andatisiro@gmail.com>
2023-06-03 20:39:54 +0300

    SpringWebApp

---
+++ src/main/java/com/springframework/spring6webapp/services/AuthorService.java
@@ -0,0 +1,7 @@
+package com.springframework.spring6webapp.services;
+
+import com.springframework.spring6webapp.domain.Author;
+
+public interface AuthorService {
+    Iterable<Author> findAll();
+}
